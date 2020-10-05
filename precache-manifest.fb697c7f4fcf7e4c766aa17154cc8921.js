72a6b881887fe311a6c9dae071d3f94e4436235f
vladan345 <vladanm087@gmail.com>
2020-10-05 20:42:35 +0200

    Updates

---
+++ precache-manifest.fb697c7f4fcf7e4c766aa17154cc8921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809e3a3e5120d97f889382c480f206fd",
+    "url": "/vladapp/index.html"
+  },
+  {
+    "revision": "f0a48776ef218fec5fc2",
+    "url": "/vladapp/static/css/main.12792e92.chunk.css"
+  },
+  {
+    "revision": "bc1e9a932597efdde4d0",
+    "url": "/vladapp/static/js/2.b1e4dee8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vladapp/static/js/2.b1e4dee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a48776ef218fec5fc2",
+    "url": "/vladapp/static/js/main.8b5c484b.chunk.js"
+  },
+  {
+    "revision": "89cc6e3afe24d48ac624",
+    "url": "/vladapp/static/js/runtime-main.517c5d4e.js"
+  }
+]);
